ebe9df006e01fe950b0a3e561884335dc8ec18d5
Matt Pierson <matt.pierson789@gmail.com>
2023-03-30 11:44:08 -0400

    test

---
+++ execution_time.rb
@@ -0,0 +1,14 @@
+
+# my_min
+# Given a list of integers find the smallest number in the list.
+
+# Example:
+
+def my_min 
+
+    my_min = list[0]
+
+end 
+
+    list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+    my_min(list)  # =>  -5
